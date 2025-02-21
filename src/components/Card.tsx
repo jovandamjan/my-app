fe1f5ebef28ca9c77237ed9aab8d588d5f3b34d2
Jovan <jovandamjan04@gmail.com>
2025-02-21 19:02:35 +0100

    commit na novom branchu

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,8 +1,18 @@
 import React from "react"
 
-export const Card = ({ data }) => {
+export const Card = ({ data }: {
+    data: {
+        title: string;
+        backdrop_path: string;
+        overview: string;
+        release_date: string;
+        video: boolean;
+        vote_average: number
+    }
+}) => {
     console.log('data', data);
     return <div><img src={`https://image.tmdb.org/t/p/w500/${data.backdrop_path}`} />
         <h2>{data.title}</h2>
+        {data.release_date}
     </div>
 }
